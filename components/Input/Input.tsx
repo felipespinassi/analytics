1d70b4e132113d8d52db795f13bb4982408a8d7e
Felipe Augusto <felipe_spinassi@hotmail.com>
2026-04-12 13:20:37 -0300

    add eye icon to see password

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,6 +1,9 @@
 import theme from "@/constants/theme";
+import { Eye, EyeOff } from "lucide-react-native";
+import { useState } from "react";
 import { TextInput } from "react-native";
 import { Box, Text } from "../RestyleComponents/RestyleComponents";
+import { TouchableOpacityBox } from "../TouchableOpacityBox/TouchableOpacityBox";
 
 type Props = {
   label?: string;
@@ -10,23 +13,45 @@ type Props = {
 };
 
 export function Input({ label, placeholder, value, onChangeText }: Props) {
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <Box>
       {label && <Text marginBottom="s">{label}</Text>}
 
-      <Box borderWidth={1} borderColor="border" borderRadius="s" bg="card">
+      <Box
+        borderWidth={1}
+        borderColor="border"
+        flexDirection="row"
+        justifyContent="space-between"
+        borderRadius="s"
+        bg="card"
+      >
         <TextInput
-          secureTextEntry={label === "Senha"}
+          secureTextEntry={label === "Senha" && !showPassword}
           style={{
             color: theme.colors.foreground,
             paddingHorizontal: theme.spacing.s,
             paddingVertical: theme.spacing.m,
+            width: "80%",
           }}
           placeholderTextColor={theme.colors.mutedForeground}
           placeholder={placeholder}
           //   value={value}
           onChangeText={onChangeText}
         />
+        {label === "Senha" && (
+          <Box justifyContent="center" padding="s">
+            {showPassword ? (
+              <TouchableOpacityBox onPress={() => setShowPassword(false)}>
+                <Eye size={20} color={theme.colors.mutedForeground} />
+              </TouchableOpacityBox>
+            ) : (
+              <TouchableOpacityBox onPress={() => setShowPassword(true)}>
+                <EyeOff size={20} color={theme.colors.mutedForeground} />
+              </TouchableOpacityBox>
+            )}
+          </Box>
+        )}
       </Box>
     </Box>
   );
